0bf4906f7f4e9cfa871ca1d3ee2bfe9450282252
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-09-09 19:23:25 -0300

    codeclimate rules[2]

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducers from '../reducers/index';
-
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(rootReducers, composeWithDevTools(applyMiddleware(thunk)));
 
