49493128f93df34eb9bb1fe8da61c6cca7b40350
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-09-09 16:05:25 -0300

    estrutura do redux feita - fetch inicial

---
+++ src/reducers/reducer.js
@@ -0,0 +1,14 @@
+
+const INITIAL_STATE = {
+  data: [],
+  loading: false,
+}
+
+export default function reducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case 'REQUEST_DATA':
+      return state;
+    default:
+      return state;
+  }
+}
---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+export function fetchMeals(tipo, input) {
+  //  tipo = s => nome da receita
+  //  tipo = f => primeira letra
+  //  tipo = i => ingrediente
+  const URL = `https://www.themealdb.com/api/json/v1/1/search.php?${tipo}=${input}`;
+  return fetch(URL).then((data) => data.json());
+}
+
+export function fetchDrinks(tipo, input) {
+  const URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?${tipo}=${input}`;
+  return fetch(URL).then((data) => data.json());
+}
